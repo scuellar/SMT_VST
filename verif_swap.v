22c63ff370e03d627cac8d92438ae8f1ecf3c2e2
Santiago <scuellar@princeton.edu>
2016-01-14 12:26:22 -0500

    I, for one, like Roman numerals.

--- verif_swap.v
+++ verif_swap.v
@@ -1,3 +1,10 @@
+(*
+To process this file you need to install Compcert and VST:
+http://vst.cs.princeton.edu/
+ *)
+
+(*The original code of swap is in swap.c *)
+(*The translated code of swap is found in swap.v*)
 Load swap.
 Require Import floyd.proofauto. 
 Require Import floyd.extract_smt.
@@ -7,7 +14,10 @@ Proof. make_compspecs prog. Defined.
 
 Local Open Scope logic.
 
-
+(*Hoare style specification of the function swap*)
+(* WITH close specifies the quantified variables*)
+(* PRE specidies the precondition which includes PROP LOCAL and SEP*)
+(* POST specidies the postcondition which includes PROP LOCAL and SEP*)
 Definition swap_spec :=
  DECLARE _swap
   WITH a: val, sh: share, contents: list int, i : Z, j : Z, size: Z
@@ -22,17 +32,25 @@ Definition swap_spec :=
    LOCAL()
    SEP (data_at sh (tarray tint size) (map Vint contents') a).
 
+(* The specification of main is irrelevant*)
 Definition main_spec := 
  DECLARE _main
   WITH u : unit
   PRE  [] main_pre prog u
   POST [ tint ] main_post prog u.
 
-
+(* Define the global variables and all the functions specifications of the program*)
 Definition Vprog : varspecs := nil.
 Definition Gprog : funspecs :=
   swap_spec :: main_spec :: nil.
 
+(* Behold the verification of the swap function *)
+(* The comments correspond to steps in the swap function 
+   or subgoals extracted to the SMT solver *)
+(* All the admits correspond to steps we manually extract
+   into CVC4 to verify automatically *)
+(* If CVC4 produced proof winesses, we could reflect them back
+   and use them to complete the this machine-checked proof. *)
 Lemma body_swap : semax_body Vprog Gprog f_swap swap_spec.
 Proof.
   start_function.
@@ -59,6 +77,8 @@ Proof.
   rewrite upd_Znth_map. 
   rewrite upd_Znth_map. 
   entailer.
+  (* The three subgoals described in the report
+     come out of the following extraction: *)
   extract_smt. (*SMT solve*) admit.
   extract_smt. (*SMT solve*) admit.
   extract_smt. (*SMT solve*) admit.
